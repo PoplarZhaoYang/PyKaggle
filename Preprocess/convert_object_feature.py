110defe90225719813eee06b03852479fc6fb484
jibancanyang <1599664856@qq.com>
2017-07-05 23:16:06 +0800

    add label

---
+++ Preprocess/convert_object_feature.py
@@ -0,0 +1,11 @@
+
+def convert_object_into_label(df):
+    """label the object feature
+    """
+    df_num = df.select_dtypes(exclude=['object'])
+    df_obj = df.select_dtypes(include=['object']).copy()
+    for c in df_obj:
+        df_obj[c] = pd.factorize(df_obj[c])[0]
+    return pd.concat([df_num, df_obj], axis=1)
+
+
