97bfe8088d06596186f575ba138f947b39553148
jibancanyang <1599664856@qq.com>
2017-11-26 18:48:47 +0800

    add desc

--- Preprocess/convert_object_feature.py
+++ Preprocess/convert_object_feature.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 import pandas as pd
+
 
 def convert_object_into_label(df):
     """label the object feature
@@ -8,5 +11,6 @@ def convert_object_into_label(df):
     for c in df_obj:
         df_obj[c] = pd.factorize(df_obj[c])[0]
     return pd.concat([df_num, df_obj], axis=1)
+
 
 
